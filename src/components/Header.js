6cacad4e2e4c2e12ed967b2a3dd8d23107af1972
Sibints2000 <sibints2000@gmail.com>
2021-09-11 17:54:03 +0530

    Eleventh Commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -18,4 +18,10 @@ Header.propTypes = {
     title: PropTypes.string.isRequired,
 }
 
+// CSS in JS
+// const headingStyle = {
+//     color: 'red', 
+//     backgroundColor: 'black'
+// }
+
 export default Header
