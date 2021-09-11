5236db54583796c3e7ebe738e0c5dbfc10f7c2cc
Sibints2000 <sibints2000@gmail.com>
2021-09-11 19:03:00 +0530

    Thirty Second Commit

--- src/App.js
+++ src/App.js
@@ -33,7 +33,7 @@ function App() {
   return (
     <div className="container">
       <Header />
-      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} /> : ''}
+      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} /> : 'No Tasks to Show'}
     </div>
   );
 }
