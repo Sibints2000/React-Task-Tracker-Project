a79f1a00fc7aee5b541b5ee870e5085a52aefa66
Sibints2000 <sibints2000@gmail.com>
2021-09-11 20:54:55 +0530

    Thirty NInth Commit

--- src/App.js
+++ src/App.js
@@ -26,6 +26,13 @@ function App() {
     }
   ])
 
+  // Add Task
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1
+
+    console.log(id);
+  }
+
   // Delete Task
   const deleteTask = (id) => {
     setTasks(tasks.filter((task) => task.id !== id))
@@ -40,7 +47,7 @@ function App() {
   return (
     <div className="container">
       <Header />
-      <AddTask />
+      <AddTask onAddTask={addTask} />
       {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} /> : 'No Tasks to Show'}
     </div>
   );
