434f59f63939dae7a854b0815884d0aae5b922de
Sibints2000 <sibints2000@gmail.com>
2021-09-11 18:54:00 +0530

    Thirtieth Commit

--- src/App.js
+++ src/App.js
@@ -23,12 +23,17 @@ function App() {
         day: 'Feb 5th at 2:30pm',
         reminder: false,
     }
-])
+  ])
+
+  // Delete Task
+  const deleteTask = (id) => {
+    console.log('delete', id);
+  }
 
   return (
     <div className="container">
       <Header />
-      <Tasks tasks={tasks} />
+      <Tasks tasks={tasks} onDelete={deleteTask} />
     </div>
   );
 }
